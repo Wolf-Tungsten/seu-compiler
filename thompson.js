ca687b60d5cdbc6a0e6db89a2545565a46401a41
Wolf-Tungsten <canis.tungsten@foxmail.com>
2018-12-31 15:40:11 +0800

    约定automa表示方式，完成字符、或操作符的thompson算法部分

--- thompson.js
+++ thompson.js
@@ -28,4 +28,49 @@ function postfix(exp){
     return output.join('')
 }
 
+function thompson(postfixExp, stateBias){
+    let automaStack = []
+    let stateNo = stateBias
+    let pointer = 0
+    while(pointer < postfixExp.length){
+        if(postfixExp[pointer]==='\\'){
+            let startState = stateNo++
+            let endState = stateNo++
+            let automa = {
+                start: `S${startState}`,
+                end: `S${endState}`,
+            }
+            automa.stateList = [automa.start, automa.end]
+            automa[automa.start] = {'@':[]}
+            automa[automa.end] = {'@':[]}
+            automa[automa.start][`\\${postfixExp[pointer+1]}`] = automa.end
+            automaStack.push(automa)
+            pointer += 2
+            continue
+        }
+        if(postfixExp[pointer]==='|'){
+            let top_1 = automaStack.pop()
+            let top_2 = automaStack.pop()
+            let startState = stateNo++
+            let endState = stateNo++
+            let automa = {
+                start: `S${startState}`,
+                end: `S${endState}`
+            }
+            automa.stateList = ([automa.start, automa.end]).concat(top_1.stateList, top_2.stateList)
+            automa[automa.start]={'@':[top_1.start, top_2.start]}
+            automa[automa.end] = {'@':[]}
+            top_1.stateList.forEach(k => {
+                automa[k] = top_1[k]
+            })
+            top_2.stateList.forEach(k => {
+                automa[k] = top_2[k]
+            })
+            automa[top_1.end]['@'].push(automa.end)
+            automa[top_2.end]['@'].push(automa.end)
+            pointer+=2
+            continue
+        }
+    }
+}
 console.log(postfix('((a|(b)))*'))
